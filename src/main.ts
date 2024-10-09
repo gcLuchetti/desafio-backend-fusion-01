0c1f0010e0933efe71e3dc30357f47bcd4aaf282
luchetti <carvalho_luchetti@hotmail.com>
2024-10-09 18:22:50 -0300

    remove exists from main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { exists, existsSync, unlinkSync } from 'fs';
+import { existsSync, unlinkSync } from 'fs';
 
 async function bootstrap() {
   const dbFile = 'db.sqlite';
