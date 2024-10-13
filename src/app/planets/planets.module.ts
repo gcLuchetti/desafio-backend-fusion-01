604b6d40c039c997448efdc1e3af457426727a77
luchetti <carvalho_luchetti@hotmail.com>
2024-10-13 20:13:01 -0300

    import Planet Entity

--- src/app/planets/planets.module.ts
+++ src/app/planets/planets.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PlanetsService } from './planets.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Planet } from './entities/planet.entity'
 import { PlanetsController } from './planets.controller';
+import { PlanetsService } from './planets.service';
 
 @Module({
-  controllers: [PlanetsController],
-  providers: [PlanetsService],
+    imports: [TypeOrmModule.forFeature([Planet])],
+    controllers: [PlanetsController],
+    providers: [PlanetsService],
+    exports: [PlanetsService]
 })
 export class PlanetsModule {}
