58d6faf83d4e9a9e9bd835b104deabcacd218879
luchetti <carvalho_luchetti@hotmail.com>
2024-10-13 21:09:48 -0300

    add Validation - ValidationPipe to the app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { existsSync, unlinkSync } from 'fs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const dbFile = 'db.sqlite';
   if(existsSync(dbFile)) unlinkSync(dbFile);
 
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
