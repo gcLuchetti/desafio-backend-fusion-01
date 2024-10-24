aea4e002ac08f66d80c676a7f1d55be7f3d2a3ea
luchetti <carvalho_luchetti@hotmail.com>
2024-10-23 23:38:17 -0300

    add info to the swagger documentation

--- src/app/spaceships/dto/create-spaceship.dto.ts
+++ src/app/spaceships/dto/create-spaceship.dto.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
 
 export class CreateSpaceshipDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   model: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   manufacturer: string;
 
   @IsNotEmpty()
   @IsInt()
   @Min(1)
+  @ApiProperty()
   passengerCapacity: number;
 }
--- src/app/spaceships/spaceships.controller.ts
+++ src/app/spaceships/spaceships.controller.ts
@@ -7,33 +7,48 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { SpaceshipsService } from './spaceships.service';
 import { CreateSpaceshipDto } from './dto/create-spaceship.dto';
 import { UpdateSpaceshipDto } from './dto/update-spaceship.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('spaceships')
 @ApiTags('Spaceships')
 export class SpaceshipsController {
   constructor(private readonly spaceshipsService: SpaceshipsService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Creates a new spaceship in the system.',
+  })
   async create(@Body() createSpaceshipDto: CreateSpaceshipDto) {
     return await this.spaceshipsService.create(createSpaceshipDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Returns a list of all registered spaceships.',
+  })
   async findAll() {
     return await this.spaceshipsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Retrieves information about a specific spaceship.',
+  })
   async findOne(@Param('id', new ParseIntPipe()) id: number) {
     return await this.spaceshipsService.findOneByIdOrFail(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Updates the information of a specific spaceship.',
+  })
   async update(
     @Param('id', new ParseIntPipe()) id: number,
     @Body() updateSpaceshipDto: UpdateSpaceshipDto,
@@ -42,6 +57,9 @@ export class SpaceshipsController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Removes a spaceship from the system.',
+  })
   async remove(@Param('id', new ParseIntPipe()) id: number) {
     return await this.spaceshipsService.remove(id);
   }
