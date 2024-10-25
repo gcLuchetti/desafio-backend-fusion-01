ece6a0c10103404c8f6d1f64a2b0bd0c896c02f4
luchetti <carvalho_luchetti@hotmail.com>
2024-10-25 11:41:13 -0300

    create an data-source to use migrations

---
+++ src/database/data-source.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from "@nestjs/config";
+import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const configService = new ConfigService();
+
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get('POSTGRES_HOST'),
+  port: parseInt(configService.get('POSTGRES_PORT')),
+  username: configService.get('POSTGRES_USER'),
+  password: configService.get('POSTGRES_PASSWORD'),
+  database: configService.get('POSTGRES_DB'),
+  synchronize: false,
+  logging: true,
+  entities: [__dirname + '/app/**/entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+});
+
+export default dataSource;
